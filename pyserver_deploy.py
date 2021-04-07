1dc3f2aaad3c1e9023eeabc16e175faa623d143c
ferdiokt <63292471+ferdiokt@users.noreply.github.com>
2021-04-08 03:35:31 +0700

    fixing postgres

--- pyserver_deploy.py
+++ pyserver_deploy.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jajfghehqmvhty:d53d646f3ad54fbc4dbe08f1ee4e74390ba45b85f89a38718ae2fc4f4bafff88@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d9k5j118537rok?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jajfghehqmvhty:d53d646f3ad54fbc4dbe08f1ee4e74390ba45b85f89a38718ae2fc4f4bafff88@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d9k5j118537rok?sslmode=require'
 db = SQLAlchemy(app)
 
 
@@ -22,8 +22,13 @@ class Data(db.Model):
 
 
 @app.route('/')
-@app.route('/index', methods= ['POST'])
-def index():
+@app.route('/index')
+def index(): 
+    return render_template('index.html')
+
+
+@app.route('/success', methods= ['POST'])
+def success():
     if request.method == 'POST':
         req_name = request.form['name']
         req_email = request.form['email']
@@ -33,10 +38,7 @@ def index():
         data = Data(req_name, req_email, req_subject, req_message)
         db.session.add(data)
         db.session.commit()            
-        return render_template('index.html', text= 'Success! I will contact you shortly!')
-    
-    return render_template('index.html')
-
+        return render_template('success.html')
 
 if (__name__) == ('__main__'):
     app.run(debug=True)
